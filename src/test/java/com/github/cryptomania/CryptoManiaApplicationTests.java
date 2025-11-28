e86205fb73c1873052fc79212dff2690ea71c204
= <=>
2025-11-28 07:30:05 +0100

    chore: init

---
+++ src/test/java/com/github/cryptomania/CryptoManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cryptomania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
